3e9128fc74627c7048d69f1077e7686d846c99b4
radovanovic44 <danielradovanovic44@gmail.com>
2026-01-14 23:52:31 +0100

    provera

---
+++ Domain/PomocneMetode/GeneratorSifreVina.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.PomocneMetode
+{
+    public class GeneratorSifreVina
+    {
+        public static string GenerisiSifruVina(Guid IDvina)
+        {
+            string Id_naziv=IDvina.ToString();
+            string godina = datumFlasiranja.Year.ToString();
+            
+            string sifraVina = $"VN-{godina}-{Id_naziv}";
+            return sifraVina;
+        }
+    }
+}
